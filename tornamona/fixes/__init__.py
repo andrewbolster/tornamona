8b33b09f314e3b9af1e8f3e2d13d19bbbd2f78e2
Andrew Bolster <me@andrewbolster.info>
2020-04-25 16:21:16 +0100

    Initial commit, minimum viable nisra weekly deaths fix

---
+++ tornamona/fixes/__init__.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from typing import Optional, List, Union, Dict, AnyStr
+
+
+class Dataset:
+    fixes: List = []
+    data: Optional[Union[Dict, pd.DataFrame, pd.Series]] = None
+    sources: Union[AnyStr, Dict] = None
+
+    def register_fixes(self):
+        pass
+
+    def get(self, **kwargs) -> 'Dataset':
+        ...
+        return self
+
+    def clean(self) -> 'Dataset':
+        ...
+        return self
+
+    def to_df(self) -> pd.DataFrame:
+        if isinstance(pd.DataFrame, self.data):
+            return self.data
+        else: # assume it's something concatable
+            return pd.concat(self.data)
